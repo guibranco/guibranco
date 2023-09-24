3c11449bd3fb27c68729ff6bf2812c05bc637bb8
Guilherme Branco Stracini <guilhermes@outsurance.ie>
2023-09-24 01:07:03 +0100

    Fix experience

--- .github/automations/experience.js
+++ .github/automations/experience.js
@@ -1,16 +1,38 @@
 const fs = require('fs');
-const calculateAge = require('calculate-age').default
+const { ReadmeBox } = require('readme-box');
+const chunk = require('chunk');
+const calculateAge = require('calculate-age').default;
 const fileName = '.github/automations/experience.json';
 
-function main() {
+const columns = 5;
+
+const generateCell = (data) => `<td><strong>${calculateAge(data.since).getObject().years}+</strong> ${data.tech}</td>`;
+
+const generateRow = (data) => {
+    const cells = data.map((cell) => generateCell(cell));
+
+    if (cells.length < columns) {
+        cells.push('<td></td>'.repeat(columns - cells.length));
+    }
+
+    return `<tr>${cells.join('')}</tr>`;
+}
+
+async function main() {
     try {
-        const strData = fs.readFileSync(fileName, 'utf8');
-        const data = JSON.parse(strData);
-        for (const element of data) {
-            element.age = calculateAge(element.since).getObject().years;
-        }
-        const result = JSON.stringify(data);
-        fs.writeFileSync(fileName, result);
+        const data = fs.readFileSync(fileName, 'utf8');
+        const json = JSON.parse(data);
+        const rows = chunk(json, columns).map((row) => generateRow(row));
+        const html = `<table width="100%"><thead><th colspan="5">Experience</th></thead><tbody>${rows.join('')}</tbody></table>`;
+
+        await ReadmeBox.updateSection(html, {
+            owner: process.env.GITHUB_REPOSITORY.split('/')[0],
+            repo: process.env.GITHUB_REPOSITORY.split('/')[1],
+            token: process.env.GITHUB_TOKEN,
+            branch: 'main',
+            section: 'experience-section',
+            emptyCommits: false
+        });
     } catch (err) {
         console.error(err);
     }
