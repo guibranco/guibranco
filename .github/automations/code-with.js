10c43551c674b6180b55fe64d78ed7763aa70c7e
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-04-12 21:43:40 +0100

    Add code with

--- .github/automations/code-with.js
+++ .github/automations/code-with.js
@@ -1,30 +1,41 @@
-const fs = require('fs');
-const { ReadmeBox } = require('readme-box');
-const chunk = require('chunk');
-const fileName = '.github/automations/code-with.json';
-
-const generateRow = (data) => {
-  return data;
+const fs = require("fs");
+const simpleIcon = require("simple-icons");
+const fileName = ".github/automations/code-with.json";
+const generateImage = (tech) => {
+  if (simpleIcon["si" + tech.slug]) {
+    const icon = simpleIcon["si" + tech.slug];
+    return `<img alt="${tech.name}" src="https://img.shields.io/badge/-${tech.text}-${icon.hex}?style=flat-square&logo=${icon.slug}&logoColor=white" />`;
+  }
+  console.error(`Icon not found for ${tech.name}`);
+  return tech.name;
 };
 
-async function main() {
-    try {
-        const data = fs.readFileSync(fileName, 'utf8');
-        const json = JSON.parse(data);
-        const rows = chunk(json, columns).map((row) => generateRow(row));
-        const html = `<table width="100%"><thead><th colspan="5">Experience (in years)</th></thead><tbody>${rows.join('')}</tbody></table>`;
+const generateRow = (data) => {
+  const techs = data.technologies.map((tech) => generateImage(tech));
+  return `<tr><th>${data.skill}</th><td>${techs.join(" ")}</td></tr>`;
+};
 
-        await ReadmeBox.updateSection(html, {
-            owner: process.env.GITHUB_REPOSITORY.split('/')[0],
-            repo: process.env.GITHUB_REPOSITORY.split('/')[1],
-            token: process.env.GITHUB_TOKEN,
-            branch: 'main',
-            section: 'code-with-section',
-            emptyCommits: false
-        });
-    } catch (err) {
-        console.error(err);
-    }
+
+async function main() {
+  try {
+    const data = fs.readFileSync(fileName, "utf8");
+    const json = JSON.parse(data);
+    const rows = json.map((row) => generateRow(row));
+    const html = `<table width="100%"><thead><th>Skill</th><th>Technologies</th></thead><tbody>${rows.join(
+      ""
+    )}</tbody></table>`;
+
+    await ReadmeBox.updateSection(html, {
+      owner: process.env.GITHUB_REPOSITORY.split("/")[0],
+      repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+      token: process.env.GITHUB_TOKEN,
+      branch: "main",
+      section: "code-with-section",
+      emptyCommits: false,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 main();
