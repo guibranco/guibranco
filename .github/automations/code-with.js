e8d33f1d0f5c8afe097cfb75c2bf250f20867a88
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-04-12 21:51:18 +0100

    Fix code smells

--- .github/automations/code-with.js
+++ .github/automations/code-with.js
@@ -16,25 +16,21 @@ const generateRow = (data) => {
 };
 
 async function main() {
-  try {
-    const data = fs.readFileSync(fileName, "utf8");
-    const json = JSON.parse(data);
-    const rows = json.map((row) => generateRow(row));
-    const html = `<table width="100%"><thead><th>Skill</th><th>Technologies</th></thead><tbody>${rows.join(
-      ""
-    )}</tbody></table>`;
+  const data = fs.readFileSync(fileName, "utf8");
+  const json = JSON.parse(data);
+  const rows = json.map((row) => generateRow(row));
+  const html = `<table width="100%"><thead><th>Skill</th><th>Technologies</th></thead><tbody>${rows.join(
+    ""
+  )}</tbody></table>`;
 
-    await ReadmeBox.updateSection(html, {
-      owner: process.env.GITHUB_REPOSITORY.split("/")[0],
-      repo: process.env.GITHUB_REPOSITORY.split("/")[1],
-      token: process.env.GITHUB_TOKEN,
-      branch: "main",
-      section: "code-with-section",
-      emptyCommits: false,
-    });
-  } catch (err) {
-    console.error(err);
-  }
+  await ReadmeBox.updateSection(html, {
+    owner: process.env.GITHUB_REPOSITORY.split("/")[0],
+    repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+    token: process.env.GITHUB_TOKEN,
+    branch: "main",
+    section: "code-with-section",
+    emptyCommits: false,
+  });
 }
 
 main();
